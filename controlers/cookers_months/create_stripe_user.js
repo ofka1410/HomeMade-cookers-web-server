9b7d6a3dfda6bfb44f5d7fd0d6ba75080aa58d16
ofka1410 <ofekme19@gmail.com>
2021-08-04 18:10:27 +0300

    login update

--- controlers/cookers_months/create_stripe_user.js
+++ controlers/cookers_months/create_stripe_user.js
@@ -5,8 +5,8 @@ exports.create_card = async(req,res)=>{
    try{
  const token= process.env.WISE_TOKEN 
  console.log(token)
-   const res = await fetch(`https://api.sandbox.transferwise.tech/v1/profiles \
-   -H Authorization: Bearer ${token}`)  
+   const res = await fetch(`https://api.sandbox.transferwise.tech/v1/profiles\
+   -H Authorization: Bearer a9a4bdb5-4521-4728-a031-a1e15e4dc5e`)  
   const data= await res.json()
   console.log(data)
 res.send({token:data })
