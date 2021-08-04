0a450762bfde367c7dbf67dd8a7c69570b0b3b75
ofka1410 <ofekme19@gmail.com>
2021-08-04 10:35:22 +0300

    login update

--- routes/cookers_manage/Meals_route.js
+++
@@ -1,9 +0,0 @@
-const express= require('express')
-const get_dishes= require('../../controlers/avialable_dish/get_dish')
-const changed_avialable = require('../../controlers/avialable_dish/change_available')
-const change_all = require('../../controlers/avialable_dish/change_all')
-const router = express.Router();
-router.route("/:id").get(get_dishes.get_dishes);
-router.route("/").put(changed_avialable.change_dishes);
-router.route("/").post(change_all.change_all);
-module.exports = router;
