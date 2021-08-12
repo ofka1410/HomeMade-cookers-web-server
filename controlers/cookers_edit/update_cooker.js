34280187dacdbe69310e0143d6f07a61a303bf7a
ofka1410 <ofekme19@gmail.com>
2021-08-12 17:04:41 +0300

    login update

--- controlers/cookers_edit/update_cooker.js
+++ controlers/cookers_edit/update_cooker.js
@@ -3,14 +3,15 @@ const admin = require('../../admin_firebase')
 exports.update_cooker = async(req,res)=>{
     const cookers = db.collection('cookers')
     const data= req.body
-    const story = db.collection('cookers')
+
     try{
         console.log(data.phone_number)
         let snapshot = await cookers.doc(data.id).update({
             address:data.address,
             phone_number:data.phone_number,
             Email:data.email,
-            about:data.story 
+            about:data.story,
+             working_hours:data.cooker_working
         })
         
 
@@ -19,9 +20,7 @@ snapshot = await db.collection('cookers').get()
 snapshot.forEach(doc=>{
     cookers_update.push({...doc.data(),id:doc.id})
 })
-snapshot= story.where('cooker_id',"==", data.id).update({
-    story:data.story
-})
+
 res.send({status:'success',cookers_update})
 
     }
