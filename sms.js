e8bd7cbf2b918a406351af797c97b7ef72c810ce
ofka1410 <ofekme19@gmail.com>
2021-08-15 11:32:31 +0300

    login update

--- sms.js
+++ sms.js
@@ -11,18 +11,16 @@ var futureDate = new Date(currentDate.getTime() - minutesToAdd*60000);
       
       // let x=data.delivery_time
        theDate = new Date(futureDate* 1000)
-        let all_resevaition=`  מזל טוב, התקבלה הזמנה חדשה! \n ההזמנה מופיעה במלואה באפליקציה וחובה להכנס ולהזין זמן משלוח !.\n\n` 
-        all_resevaition+=` ההזמנה לתאריך:${new Date(theDate).toLocaleString(undefined,{
-          month: "long", day: "numeric", 
-          hour: "numeric", minute:"numeric"})}\n` 
+        let all_resevaition=`  מזל טוב, התקבלה הזמנה חדשה!\n\n` 
+        all_resevaition+=` למתי: ${new Date(data.delivery).toLocaleString('en-GB',{ hour: "numeric", minute:"numeric",
+        month: "long", day: "numeric"
+       })}\n` 
         all_resevaition+='פרטי משלוח:\n'
         all_resevaition+=`שם:${data.full_name ||"לא נשלח שם"}.\nעיר:${data.city ||"לא צוין עיר"}.\nרחוב:${data.address}.\nמספר טלפון:${data.phone||"לא נשלח מספר"}`
         for(let i=0;i<data.items.length;i++){
             all_resevaition= all_resevaition =all_resevaition + `${[i+1]}.\nשם המנה: ${data.items[i].name}. כמות: ${data.items[i].amount}.\nמחיר סופי: ${data.items[i].price}₪.\n`
              }
              all_resevaition+=`הערות לטבח: ${data.comment ||"אין הערות"}.\n\n`
-             all_resevaition+= `שעת משלוח: ${data.arrival_time}.\n`
-             all_resevaition+= `מחיר ההזמנה: ${data.total_price}.\n`
         let cookers=[]
          snapshot = await db.collection('cookers').get()
         snapshot.forEach(doc=>{
@@ -35,9 +33,9 @@ var futureDate = new Date(currentDate.getTime() - minutesToAdd*60000);
    
         let total_price=0
         let uniqe_msg='  מזל טוב, התקבלה הזמנה חדשה! \n ההזמנה מופיעה במלואה באפליקציה וחובה להכנס ולהזין זמן משלוח !.\n\n'
-         uniqe_msg+= `: ההזמנה לתאריך ${new Date(theDate).toLocaleString(undefined,{
-          month: "long", day: "numeric", 
-          hour: "numeric", minute:"numeric"})}\n` 
+         uniqe_msg+= ` למתי: ${new Date(el.delivery).toLocaleString('en-GB',{ hour: "numeric", minute:"numeric",
+         month: "long", day: "numeric"
+        })}\n` 
           for(let j=0;j<data.items.length;j++){
            
             if(data.items[j].cooker_id === cookers[i].id ){
@@ -48,8 +46,6 @@ var futureDate = new Date(currentDate.getTime() - minutesToAdd*60000);
           let id=data.items.filter(el=>el.cooker_id === cookers[i].id)
           if(id.length>0){
             uniqe_msg+=`הערות: ${data.comment ||"אין הערות"}.\n\n`
-            uniqe_msg+=`שעת משלוח: ${data.arrival_time}.\n`
-            uniqe_msg += `מחיר הזמנה : ${total_price}₪.\n`
             await client.messages.create({
               body:uniqe_msg,
               from:'(952) 260-5618',
